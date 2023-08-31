080ded6872946131e2bc4b89c46b3e654e1d630e
thianlianben-gs <thianlianben@globalwalkers.co.jp>
2023-08-31 16:47:34 +0630

    Add publisher and Displaying Image from ROS2 message

--- lib/src/display_page.dart
+++ lib/src/display_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ros_yolox_app/components/TextButtonComponent.dart';
+import 'package:roslibdart/roslibdart.dart';
 
 class DisplayPage extends StatefulWidget {
   const DisplayPage({super.key});
@@ -10,19 +14,71 @@ class DisplayPage extends StatefulWidget {
 }
 
 class _DisplayPageState extends State<DisplayPage> {
+  late Ros ros;
+  late Topic chatter;
+  late Topic publisher_chatter;
+  Uint8List? _latestImageData;
+
+  StreamController<Uint8List> imageStreamController =
+      StreamController<Uint8List>();
+
   @override
   void initState() {
-    //set initial Orientation to landscape
+    ros = Ros(url: 'ws://10.0.2.2:9090');
+    chatter = Topic(
+        ros: ros,
+        name: '/image_publisher',
+        type: "sensor_msgs/CompressedImage",
+        reconnectOnClose: true,
+        queueLength: 10,
+        queueSize: 10);
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeLeft,
       DeviceOrientation.landscapeRight,
     ]);
+    publisher_chatter = Topic(
+        ros: ros,
+        name: '/end_system',
+        type: 'std_msgs/String',
+        reconnectOnClose: true,
+        queueLength: 10,
+        queueSize: 10);
     super.initState();
+    ros.connect();
+    initConnection();
+    // Timer(const Duration(seconds: 1), () async {
+    //   await chatter.subscribe(subscribeHandler);
+    //   // await chatter.subscribe();
+    // });
+  }
+
+  void initConnection() async {
+    await chatter.subscribe(subscribeHandler);
+  }
+
+  void destroyConnection() async {
+    await chatter.unsubscribe();
+    await ros.close();
+    imageStreamController.close();
+  }
+
+  dynamic msgReceived;
+  Future<void> subscribeHandler(msg) async {
+    _latestImageData = base64.decode(msg['data']);
+    imageStreamController.add(base64.decode(msg['data']));
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    destroyConnection();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body:
       body: OrientationBuilder(
           builder: (context, orientation) => SafeArea(
                 child: Column(
@@ -40,22 +96,50 @@ class _DisplayPageState extends State<DisplayPage> {
                       ),
                     ),
                     Expanded(
-                      child: Row(
+                      child: Column(
                         children: [
                           Container(
-                              // width: double.infinity,
-                              padding: EdgeInsets.all(5.0),
-                              child: Image(
-                                image: AssetImage("assets/placeholder.png"),
-                              )),
+                            // width: double.infinity,
+                            padding: EdgeInsets.all(5.0),
+                            child: StreamBuilder(
+                              stream: imageStreamController.stream,
+                              builder: ((context, snapshot) {
+                                if (snapshot.hasError) {
+                                  return Center(
+                                      child: Text('Error: ${snapshot.error}'));
+                                }
+
+                                if (!snapshot.hasData) {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                }
+
+                                if (snapshot.hasData) {
+                                  final imageDataToShow =
+                                      _latestImageData ?? snapshot.data;
+                                  return Center(
+                                      child: Image.memory(imageDataToShow!));
+                                }
+                                return Center(
+                                  child: Text("Hello"),
+                                );
+                              }),
+                            ),
+                          ),
                           Container(
                               padding: EdgeInsets.all(10.0),
                               child: Column(
                                 children: [
                                   TextButtonComponent(
                                       buttonText: "End",
-                                      navigatePage: () {
-                                        print("End......");
+                                      navigatePage: () async {
+                                        Map<String, dynamic> end_json = {
+                                          "data": "end_system"
+                                        };
+                                        await publisher_chatter
+                                            .publish(end_json);
+                                        print("publishing end system");
+                                        Navigator.pop(context);
                                       })
                                 ],
                               ))
